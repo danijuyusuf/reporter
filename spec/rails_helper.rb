2c83cb6578f47cc8ba361cb811962bdf5ac3468a
andela-ydaniju <andela-ydaniju@users.noreply.github.com>
2016-04-09 17:23:18 +0100

    finish image for nav and main display

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
@@ -7,7 +6,6 @@ abort(
 ) if Rails.env.production?
 require "spec_helper"
 require "rspec/rails"
-# Add additional requires below this line. Rails is not loaded until this point!
 
 ActiveRecord::Migration.maintain_test_schema!
 
